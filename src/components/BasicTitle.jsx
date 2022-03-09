ee3c5ea2c3fe9f833be630cb92ea38cef7bfc5de
chaudhuree <chaudhuree00@gmail.com>
2022-03-10 04:57:44 +0600

    other way to access props by destructuring and use ternary operator after destructuring to style

--- src/components/BasicTitle.jsx
+++ src/components/BasicTitle.jsx
@@ -2,6 +2,10 @@ import styled from "styled-components";
 const BasicTitle = styled.h1`
   text-align: center;
   text-transform: uppercase;
-  color: ${(props)=> props.special && 'palegreen'};
+  /* color: ${(props)=> props.special && 'palegreen'}; */
+  /* destructured ⬇⬇*/
+  /* color: ${({special})=> special && 'palegreen'}; */
+  /* ternary operator ⬇⬇ */
+  color: ${({special})=> special ? 'palegreen' : 'salmon'};
 `;
 export default BasicTitle;
