a9e85947ba0fe9fcb8981848bb1bb99733b74078
chaudhuree <chaudhuree00@gmail.com>
2022-04-06 02:08:04 +0600

    docs: initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import styled form macro to use css props
 import styled from "styled-components/macro";
-import { DefaultButton, HipsterButton } from "./components/Buttons";
+import Form from "./components/Form";
 
 const Wrapper = styled.section`
   padding: 2em;
@@ -10,21 +10,7 @@ const Wrapper = styled.section`
 function App() {
   return (
     <Wrapper>
-     <HipsterButton >Click Me</HipsterButton>
-     {/* though hipster is a button element but as transform it into a link */}
-     <HipsterButton css={`color:red;`} as='a' href="googl.com">Click Me</HipsterButton>
-     <HipsterButton>Click Me</HipsterButton>
-
-{/* macro and css props */}
-{/* must use semicolon  */}
-<div css={`color:green;`}>
-  <h1>chaudhuree</h1>
-</div>
-
-{/* css helper function */}
-<DefaultButton >Click Me</DefaultButton>
-<DefaultButton large>Click Me</DefaultButton>
-    
+      <Form></Form>
     </Wrapper>
   );
 }
