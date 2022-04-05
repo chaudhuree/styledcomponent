97101fa7490e2a872e10584220ecde7addd2549c
chaudhuree <chaudhuree00@gmail.com>
2022-04-05 19:45:58 +0600

    docs: add toggle theme functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
+import React from "react";
 import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from "./global-style";
 
 const BaseTheme = {
-  color: '#222',
-  background: '#fff',
-}
+  color: "#222",
+  background: "#fff",
+};
 const DarkTheme = {
-  color: '#fff',
-  background: '#222',
-}
+  color: "#fff",
+  background: "#222",
+};
 
 const Container = styled.div`
   padding: 2rem;
   color: ${(props) => props.theme.color};
   background: ${(props) => props.theme.background};
-`
+`;
 function App() {
+  const [theme, setTheme] = React.useState(true);
+  const toggleTheme = () => {
+    setTheme(!theme);
+  };
+
   return (
-    <ThemeProvider theme={BaseTheme}>
+    <ThemeProvider theme={theme?BaseTheme:DarkTheme}>
       <GlobalStyles />
       <Container>
         <h1>chaudhuree</h1>
+        <button className="btn" onClick={toggleTheme}>toggle theme</button>
       </Container>
     </ThemeProvider>
   );
