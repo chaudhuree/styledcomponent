7b9f1d9623c2b3ea9f1c8c2c0766c3845ebd151b
chaudhuree <chaudhuree00@gmail.com>
2022-04-05 19:51:36 +0600

    docs: initial state to setup animation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import Card from "./components/Card";
-import ComplexTitle from './components/ComplexTitle';
-import Example from "./Example";
-import ExampleWithMui from "./ExampleWithMui";
-import GlobalStyles from "./global-style";
+import React from 'react';
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <Example></Example>
-      <ExampleWithMui></ExampleWithMui>
-      <ComplexTitle title='this is a title'></ComplexTitle>
-      <Card></Card>
+     
+      
     </>
   );
 }
