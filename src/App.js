5a6724f699cbeb48c713808692354c4b47844d7c
chaudhuree <chaudhuree00@gmail.com>
2022-04-05 19:37:21 +0600

    docs: initial theme setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import Card from "./components/Card";
-import ComplexTitle from './components/ComplexTitle';
-import Example from "./Example";
-import ExampleWithMui from "./ExampleWithMui";
+import styled, { ThemeProvider } from "styled-components";
 import GlobalStyles from "./global-style";
 
+const BaseTheme = {
+  color: '#222',
+  background: '#fff',
+}
+const DarkTheme = {
+  color: '#fff',
+  background: '#222',
+}
+
+const Container = styled.div`
+  padding: 2rem;
+  color: ${(props) => props.theme.color};
+  background: ${(props) => props.theme.background};
+`
 function App() {
   return (
-    <>
+    <ThemeProvider theme={BaseTheme}>
       <GlobalStyles />
-      <Example></Example>
-      <ExampleWithMui></ExampleWithMui>
-      <ComplexTitle title='this is a title'></ComplexTitle>
-      <Card></Card>
-    </>
+      <Container>
+        <h1>chaudhuree</h1>
+      </Container>
+    </ThemeProvider>
   );
 }
 
