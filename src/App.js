d429f982b0d7806030e45e4c75247c59ccce5c9f
chaudhuree <chaudhuree00@gmail.com>
2022-04-05 19:57:45 +0600

    docs: scratch setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  padding: 2em;
+`;
 
 function App() {
   return (
-    <>
-     
-      
-    </>
+    <Wrapper>
+      <h1>chaudhuree</h1>
+    </Wrapper>
   );
 }
 
