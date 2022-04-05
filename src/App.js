01667c4654e7aeaf6095793036186d1941dde28f
chaudhuree <chaudhuree00@gmail.com>
2022-04-06 03:03:05 +0600

    docs: products style design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Card from "./components/Card";
-import ComplexTitle from './components/ComplexTitle';
+import ComplexTitle from "./components/ComplexTitle";
+import Products from "./components/Products";
 import Example from "./Example";
 import ExampleWithMui from "./ExampleWithMui";
 import GlobalStyles from "./global-style";
@@ -10,8 +11,9 @@ function App() {
       <GlobalStyles />
       <Example></Example>
       <ExampleWithMui></ExampleWithMui>
-      <ComplexTitle title='this is a title'></ComplexTitle>
+      <ComplexTitle title="this is a title"></ComplexTitle>
       <Card></Card>
+      <Products></Products>
     </>
   );
 }
