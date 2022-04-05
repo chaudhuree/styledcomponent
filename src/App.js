d94821bdacf2fa16815ef1d5a74946b5aad9ee71
chaudhuree <chaudhuree00@gmail.com>
2022-04-06 01:27:16 +0600

    docs: as component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import WrapperLoading from "./components/Loading";
+import { HipsterButton } from "./components/Buttons";
 
 const Wrapper = styled.section`
   padding: 2em;
@@ -9,7 +9,12 @@ const Wrapper = styled.section`
 function App() {
   return (
     <Wrapper>
-      <WrapperLoading></WrapperLoading>
+     <HipsterButton>Click Me</HipsterButton>
+     {/* though hipster is a button element but as transform it into a link */}
+     <HipsterButton as='a' href="googl.com">Click Me</HipsterButton>
+     <HipsterButton>Click Me</HipsterButton>
+
+    
     </Wrapper>
   );
 }
